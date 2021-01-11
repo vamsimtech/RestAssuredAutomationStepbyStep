3910ca4bf846bb9f4ffe04ee15287b5d000b3cae
vamsi1 <vamsi.m569@gmail.com>
2021-01-11 14:56:17 +0530

    first commit

---
+++ RestAssuredAutomation/src/test/java/tests/JSONSchemaValidator.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+import org.testng.annotations.Test;
+
+public class JSONSchemaValidator {
+	
+		@Test
+		public void testGet() {
+			
+			baseURI ="https://reqres.in/api";
+			given().
+			get("/users?page=2").
+			then().
+			assertThat().body(matchesJsonSchemaInClasspath("schema.json")).
+			statusCode(200);
+			
+			
+		}
+
+}
---
+++ RestAssuredAutomation/src/test/java/tests/XMLSchemaValidation.java
@@ -0,0 +1,46 @@
+package tests;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import static io.restassured.matcher.RestAssuredMatchers.matchesXsdInClasspath;
+
+public class XMLSchemaValidation {
+	
+	@Test
+	public void schemaValidation() throws IOException {
+		File file= new File("./SoapReqests/add.xml");
+		
+		if(file.exists())
+			System.out.println("  >> File exists");
+		
+		FileInputStream fileInputStream = new FileInputStream(file);
+		String requestBody= IOUtils.toString(fileInputStream, "UTF-8");
+				
+		baseURI="http://www.dneonline.com";
+		given().
+			contentType("text/xml").
+			accept(ContentType.XML).
+			body(requestBody).
+		when().
+			post("/calculator.asmx").
+		then().
+			statusCode(200).log().all().
+		and().
+			body("//*:AddResult.text()",equalTo("5")).
+		and().
+			assertThat().body(matchesXsdInClasspath("caluclator.xsd"));
+	
+	}
+
+}
